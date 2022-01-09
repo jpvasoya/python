15d08fc5951d26706ff440eeeaa1ba0191e14f75
jaydip <shreekrishna9099@gmail.com>
2022-01-09 21:52:53 +0530

    colorama pip

---
+++ Practice/Practice/geeksforgeek/Array/bonus1.py
@@ -0,0 +1,7 @@
+#find sum of array
+from array import *
+arr1=array('i',[1,2,3,4,5])
+print(arr1)
+print(type(arr1))
+arr2=array('i',[1,2,3,4,5])
+print(arr1+arr2)
---
+++ pip/colorama.py
@@ -0,0 +1,10 @@
+from colorama import init
+init()
+from colorama import Back,Style,Fore
+print(Back.CYAN,"Hello")
+print(Fore.RED,"Jaydip")
+print(Style.BRIGHT,"it is a dim?")
+print("welcome")
+print("How can reset")
+print(Style.RESET_ALL)
+print(Style.DIM,"Lets check that")
